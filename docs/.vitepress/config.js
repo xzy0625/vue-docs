36047c3df5b910776ccf9cf981a8bd23fd92a7da
lebronxiong <lebronxiong@tencent.com>
2023-12-06 15:45:41 +0800

    feat: 增加lang爬虫

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -8,12 +8,18 @@ export default {
   themeConfig: {
     logo: "/images/logo.png", // 页面上显示的logo
     // algolia搜索 https://blog.csdn.net/weixin_42429718/article/details/128361258
+    // 配置详见 https://github.com/vuejs/vitepress/blob/main/types/docsearch.d.ts
+    // 这里有一个坑，vitepress会自动给请求带上facetFilter属性，所以我们在crawlerConfig中必须配置lang
     algolia: {
       appId: 'MVMNAC3H5Z',
       apiKey: '8f79ce6a0636cc5034de86b6178dfb8a', // 这里是algolia的key和indexName，请自行前往申请
       indexName: 'vueDocs',
       placeholder: '请输入关键词',
       buttonText: '搜索',
+      // searchParameters: {
+      //   attributesToRetrieve: ['*'],
+      //   attributesToSnippet: ['*:80'],
+      // }
     },
     nav: [
       // 页面右上角的导航
